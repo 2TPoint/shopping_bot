50ad7240deeafdb106d83b8c30fb6bef3769712f
Daniil <danilmega2003@gmail.com>
2023-04-24 12:28:32 +0300

    обновлен функционал, добавлены новые команды

--- main.py
+++ main.py
@@ -19,30 +19,92 @@ def start(message):
     markup.row(about_button, support_button)
     if message.text == '/start':
         bot.send_message(message.chat.id,
-                         f'Привет, {message.from_user.first_name}!\nУ меня ты сможешь купить некоторые товары!\nКонтакты моих разработчиков: @skylejke, @wJexson',
+                         f'Привет, {message.from_user.first_name}!\nЯ телеграм-бот для покупок\nУ меня ты сможешь купить некоторые товары!\nКонтакты моих разработчиков: @skylejke, @wJexson',
                          reply_markup=markup)
     else:
         bot.send_message(message.chat.id, 'Перекинул вас в главное меню', reply_markup=markup)
 
 
-@bot.message_handler(content_types=['photo'])
-def get_photo(message):
-    bot.send_message(message.chat.id, 'У меня нет возможности просматривать фото :(')
-
-
 @bot.message_handler()
 def get_info(message):
     if message.text == "📄 О боте":
-        info = "Информация о боте"
-        bot.send_message(message.chat.id, info, parse_mode='html')
+        aboutChapter(message)
     elif message.text == "🛍 Товары":
-        info = "Информация о товарах"
-        bot.send_message(message.chat.id, info, parse_mode='html')
+        productsChapter(message)
     elif message.text == "⚙ Поддержка":
-        info = "Обработка обращения"
-        bot.send_message(message.chat.id, info, parse_mode='html')
+        supportChapter(message)
+    elif message.text == '🔹 Товар №1':
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        button1 = types.KeyboardButton('💳 Купить')
+        button2 = types.KeyboardButton('↩ Назад')
+        markup.row(button1, button2)
+        bot.send_message(message.chat.id, 'Информация о первом товаре...', reply_markup=markup)
+    elif message.text == '🔹 Товар №2':
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        button1 = types.KeyboardButton('💳 Купить')
+        button2 = types.KeyboardButton('↩ Назад')
+        markup.row(button1, button2)
+        bot.send_message(message.chat.id, 'Информация о втором товаре...', reply_markup=markup)
+    elif message.text == '🔹 Товар №3':
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        button1 = types.KeyboardButton('💳 Купить')
+        button2 = types.KeyboardButton('↩ Назад')
+        markup.row(button1, button2)
+        bot.send_message(message.chat.id, 'Информация о третьем товаре...', reply_markup=markup)
+    elif message.text == '🔹 Товар №4':
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        button1 = types.KeyboardButton('💳 Купить')
+        button2 = types.KeyboardButton('↩ Назад')
+        markup.row(button1, button2)
+        bot.send_message(message.chat.id, 'Информация о четвертом товаре...', reply_markup=markup)
+    elif message.text == '✏ Написать разработчику':
+        bot.send_message(message.chat.id, '<b>Контакты моих разработчиков:</b> @skylejke, @wJexson', parse_mode='html')
+    elif message.text == '💳 Купить':
+        seller = random.randint(0, 1)
+        if seller == 0:
+            webbrowser.open('https://t.me/skylejke')
+        elif seller == 1:
+            webbrowser.open('https://t.me/wJexson')
+    elif message.text == '↩ Назад':
+        productsChapter(message)
+    elif message.text == '↩ Назад в меню':
+        start(message)
     else:
         bot.send_message(message.chat.id, wrong_answers[random.randint(0, 3)])
+
+
+def productsChapter(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    button1 = types.KeyboardButton('🔹 Товар №1')
+    button2 = types.KeyboardButton('🔹 Товар №2')
+    button3 = types.KeyboardButton('🔹 Товар №3')
+    button4 = types.KeyboardButton('🔹 Товар №4')
+    button5 = types.KeyboardButton('↩ Назад в меню')
+    markup.row(button1, button2)
+    markup.row(button3, button4)
+    markup.row(button5)
+    bot.send_message(message.chat.id, 'Раздел товаров', reply_markup=markup)
+
+
+def aboutChapter(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    button1 = types.KeyboardButton('↩ Назад в меню')
+    markup.add(button1)
+    bot.send_message(message.chat.id, 'Я телеграм-бот для покупок\nУ меня ты сможешь купить некоторые товары!',
+                     reply_markup=markup)
+
+
+def supportChapter(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
+    button1 = types.KeyboardButton('✏ Написать разработчику')
+    button2 = types.KeyboardButton('↩ Назад в меню')
+    markup.add(button1, button2)
+    bot.send_message(message.chat.id, 'Раздел поддержки', reply_markup=markup)
+
+
+@bot.message_handler(content_types=['photo'])
+def get_photo(message):
+    bot.send_message(message.chat.id, 'У меня нет возможности просматривать фото :(')
 
 
 bot.polling(none_stop=True)
